c2bb383a7995851e95d8d7c8d7a072fc87e168e3
Balint Deak <balint.deak@porsche.de>
2018-02-19 18:51:22 +0000

    adding cni initial config

--- manifests/cni/calico.pp
+++ manifests/cni/calico.pp
@@ -17,11 +17,23 @@ class kubernetes::cni::calico (
   #   mode   => '0755',
   # }
 
+  file { '/etc/cni/net.d/calico.kubeconfig':
+    ensure  => file,
+    content => epp("kubernetes/cni/calico/calico.kubeconfig.epp"),
+    replace => false,
+  }
+
+  file { '/etc/cni/net.d/10-calico.conflist':
+    ensure  => file,
+    content => epp("kubernetes/cni/calico/10-calico.conflist.epp"),
+    replace => false,
+  }
+
   wget::fetch { "install calicoctl":
     source             => 'https://github.com/projectcalico/calicoctl/releases/download/v1.6.3/calicoctl',
     destination        => '/usr/local/bin/calicoctl',
     timeout            => 60,
-    verbose            => true,
+    verbose            => false,
     nocheckcertificate => false,
     mode               => "0775",
     notify             => Exec['chmod calicoctl'],
